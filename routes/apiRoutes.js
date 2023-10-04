90acf5bc5df76084735853d990677a74201284a5
rddaps <dapa0002@umn.edu>
2023-10-04 01:49:36 -0500

    correcting .db file references and correcting apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,27 +3,28 @@ const path = require('path');
 const router = require('express').Router();
 const uniqid = require('uniqid'); 
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
-router.post('/api/notes', (req, res) => {
-        const db = JSON.parse(fs.readFileSync('../db/db.json'))
+router.post('/notes', (req, res) => {
+        const db = JSON.parse(fs.readFileSync('./db/db.json'))
         const newNote = {
             title: req.body.title,
             text: req.body.text,
             id: uniqid(),
         };
+        console.log(newNote);
     db.push(newNote);
-    fs.writeFileSync('../db/db.json', JSON.stringify(db));
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
     res.json(db);
 });
 
-router.delete('/api/notes/:id', (req, res) => {
-    const db = JSON.parse(fs.readFileSync('../db/db.json'))
-    const notes = db.filter(note => note.id !== req.params.id);
+router.delete('/notes/:id', (req, res) => {
+    const db = JSON.parse(fs.readFileSync('./db/db.json'));
+    const notes = db.filter((note) => note.id !== req.params.id);
 
-    fs.writeFileSync('../db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json('Note deleted.');
 });
 
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/',htmlRoutes);
 app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
